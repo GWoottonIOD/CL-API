1dbf19e3442334b6ddede31281a09381a78f26e7
GWootton <gareth.wootton@institutedata.com>
2025-09-29 19:05:10 +1300

    Add a mui template for a good looking front-end and I have upload button looking and axios being called. time to merge.

--- CL uploads/src/App.jsx
+++ CL uploads/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UploadFile from './components/UploadFile'
+import MarketingPage from './components/MarketingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <UploadFile />
+    <MarketingPage/>
     </>
   )
 }
--- CL uploads/src/components/UploadFile.jsx
+++ CL uploads/src/components/UploadFile.jsx
@@ -1,24 +1,47 @@
+import Button from '@mui/material/Button';
 import React from 'react'
+import axios from 'axios'
 
 export default function UploadFile() {
 
-const _handleUpload = (e) => {
+  const _handleUpload = (e) => {
     const dataForm = new FormData();
-    dataForm.append('file', e.target.files[0]);  
-      axios
-        .post('http://localhost:4000/test', dataForm)
-        .then(res => {
+    dataForm.append('file', e.target.files[0]);
+    console.log(dataForm)
+    axios
+      .post('http://localhost:4000/test', dataForm)
+      .then(res => {
 
-        })
-        .catch(err => console.log(err));      
-}
+      })
+      .catch(err => console.log(err));
+  }
 
   return (
     <div className="App">
-      <input
-      onChange={_handleUpload}
-      type="file"
-      />    
+      {/* <Button
+        component="label"
+        variant="contained"
+        color="primary"
+        size="small"
+        sx={{ alignItems: 'center' }}
+        onChange={_handleUpload}
+        type="file">
+        Upload
+      </Button> */}
+      <Button
+        component="label"
+        variant="contained"
+        color="primary"
+        size="small"
+        sx={{ alignItems: 'center' }}
+        onChange={_handleUpload}
+      >
+        Upload
+        <input
+          type="file"
+          hidden
+        />
+      </Button>
     </div>
   )
 }
